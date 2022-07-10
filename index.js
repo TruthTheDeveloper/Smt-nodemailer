57f574e4ecc2de2953dd37e514d26e9c69c718c7
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-07-10 03:52:38 +0100

    discobered the cause of the cors error

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://eyefremeng-nodemailer.herokuapp.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 
